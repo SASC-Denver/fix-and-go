d7ee03def47e45309bc9777a371a699977fd3b75
Artem Shamsutdinov <russoturiston@gmail.com>
2020-04-01 04:19:38 +0000

    removed compiled files

--- logic/lib/bootstrap.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IApp {
-}
-export interface IAppOptions {
-    target: HTMLElement;
-}
-export interface IAppConstructor {
-    new (options: IAppOptions): IApp;
-}
-export declare function bootstrap(AppConstructor: IAppConstructor): Promise<void>;
